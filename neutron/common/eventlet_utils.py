4b5ac3d1c7b484184b25df88bff030550860508d
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2017-08-09 13:50:09 +0300

    Windows: switch back to subprocess.Popen for exec calls
    At some point, we've switched to an alternative process launcher
    that uses named pipes to communicate with the child processes. This
    implementation has some issues, truncating the process output in some
    situations.
    
    This change switches back to subprocess.Popen, which is a much easier
    and convenient way to perform exec calls. We're also ensuring that the
    os module is not patched (which would cause subprocess.Popen to fail
    on Windows due to an eventlet limitation, the reason why the above
    mentioned implementation was first introduced).
    
    We're also ensuring that such calls do not block other greenthreads
    by leveraging eventlet.tpool.
    
    Closes-Bug: #1709931
    
    Change-Id: Ib58e12030e69ea10862452c2f141a7a5f2527621

--- neutron/common/eventlet_utils.py
+++ neutron/common/eventlet_utils.py
@@ -20,7 +20,13 @@ from oslo_utils import importutils
 
 
 def monkey_patch():
-    eventlet.monkey_patch()
     if os.name != 'nt':
+        eventlet.monkey_patch()
+
         p_c_e = importutils.import_module('pyroute2.config.asyncio')
         p_c_e.asyncio_config()
+    else:
+        # eventlet monkey patching the os module causes subprocess.Popen to
+        # fail on Windows when using pipes due to missing non-blocking IO
+        # support.
+        eventlet.monkey_patch(os=False)
